7c2de59b166d78c0cd84f3f6504ec85cee92f7d2
Dave Lunny <sir.dave@me.com>
2014-07-23 21:25:05 -0400

    Super-basic functionality
    Like I mean really really really basic stuff. Prompts and alerts. But
    I've done a few important things here, including setting up the
    leveling, and adding/clearing localstorage, creating the player
    constructor.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,63 @@
 //	Written by Dave Lunny
+function init(){	
 
-function		init()		{
-	
-	
-	
-	
-};//	End init function
+	//Level-mapping, for later use
+	/*
+	var x;
+	var thisXp = 1
+	for( x in levelsMap ){
+		thisXp = levelsMap[x];
+	};		
+	*/
+
+	setupForgetMe();
+
+	//First, finna check if the user exists:
+	var player = localStorage.getItem('player');
+
+	if(player){
+		console.log('we got a player');
+		var p = JSON.parse(player);
+	}else{
+		console.log('aint no player');
+		var p = setupPlayer();
+		localStorage.setItem('player',JSON.stringify(p));
+	}
+
+	setDeets(p);
+
+};
 
 
 
+
+
+function setupPlayer() 	{
+
+	var n = prompt("Please enter your name", "");
+	var p = new Player(n);
+	return p;
+};
+
+function setDeets(p)	{
+
+	$('#name').html(p.name);
+	$('#lvl').html(p.lvl);
+	$('#XP').html(p.xp);
+	$('#money').html(p.money);
+	$('#day').html(p.day);
+
+};
+
+function setupForgetMe() {
+
+	$('#forget').on('click',function(){
+		alert('You are forgotten. Refreshing the page');
+		localStorage.clear();
+		location.reload();
+	})
+
+};
 
 
 
---
+++ js/setup.js
@@ -0,0 +1,43 @@
+/*		WELCOME TO THE SETUP FOR DEV TYCOON		*/
+
+//	Player constructor
+function Player(name){
+	this.name  	= name;
+	this.lvl   	= 1;
+	this.xp	   	= 75;
+	this.money 	= 10000;
+	this.devcap = 1;
+	this.day	= 1;
+};
+
+var lvlXpMult = 1.61803398875;
+// LevelsMap holds all data regarding how much XP is needed to reach each level
+levelsMap = {
+
+		lvl1: 	75 	
+	,	lvl2: 	Math.pow(lvlXpMult, 9.5)
+	,	lvl3: 	Math.pow(lvlXpMult, 10)
+	,	lvl4: 	Math.pow(lvlXpMult, 10.5)
+	,	lvl5: 	Math.pow(lvlXpMult, 11)
+	,	lvl6: 	Math.pow(lvlXpMult, 11.5)
+	,	lvl7: 	Math.pow(lvlXpMult, 12)
+	,	lvl8: 	Math.pow(lvlXpMult, 12.5)
+	,	lvl9: 	Math.pow(lvlXpMult, 13)
+	,	lvl10: 	Math.pow(lvlXpMult, 13.5)
+	,	lvl11: 	Math.pow(lvlXpMult, 14)
+	,	lvl12: 	Math.pow(lvlXpMult, 14.5)
+	,	lvl13: 	Math.pow(lvlXpMult, 15)
+	,	lvl14: 	Math.pow(lvlXpMult, 15.5)
+	,	lvl15: 	Math.pow(lvlXpMult, 16)
+	,	lvl16: 	Math.pow(lvlXpMult, 16.5)
+	,	lvl17: 	Math.pow(lvlXpMult, 17)
+	,	lvl18: 	Math.pow(lvlXpMult, 17.5)
+	,	lvl19: 	Math.pow(lvlXpMult, 18)
+	,	lvl20: 	Math.pow(lvlXpMult, 18.5)
+
+};
+
+
+
+
+
