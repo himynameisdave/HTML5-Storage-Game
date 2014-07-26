1e33ce9eaa20d9a21e9016a0ed3b0a33c6ee9c25
Dave Lunny <sir.dave@me.com>
2014-07-26 18:33:18 -0400

    Major code reorg
    Reorganized the code so it is a lot cleaner, also added some sweet
    comments so that everything makes (a little) more sense

--- js/script.js
+++ js/script.js
@@ -11,97 +11,51 @@ function init(){
 	*/
 
 
-
 	//First, finna check if the user exists:
 	var player = localStorage.getItem('player');
 
 	if(player){
 		var p = JSON.parse(player);
-		btnSetup(p);
 	}else{
 		var p = setupPlayer();
 		storeDeets(p);
-		btnSetup(p);
 	}
 
-	updateDeets(p);
+	btnSetup(p);
 
+	updateDeets(p);
+	updateProjects(p);
 };
 
 
 
+/*/////////////SETUP AND STORAGE FUNCTIONS////////////*/
 
 
+//	If generating a new player, this takes the name, creates, and returns the new player object
 function setupPlayer() 	{
-
 	var n = prompt("Please enter your name", "");
 	var p = new Player(n);
 	return p;
 };
 
-function updateDeets(p)	{
+// Calls two btn setup function
+function btnSetup(p) {
+	setupForgetMe();
+	setupNxtDay(p);
+};	
 
-	$('#name').html(p.name);
-	$('#lvl').html(p.lvl);
-	$('#XP').html(p.xp);
-	$('#money').html(p.money).commaAtMeBro().prepend('$');
-	$('#day').html(p.day);
-
-};
-
-function storeDeets(p){
-	localStorage.setItem('player',JSON.stringify(p));
-};
-
-function updateProjects(p,proj){
-	//check if user has any current projects goin on
-	if(!$.isEmptyObject(p.currentProjects)){
-		
-		var len = Object.keys(p.currentProjects).length;
-		for (k = 1; k <= len; k++){
-			var i = 'proj' + k;	
-			p.currentProjects[i].daysLeft--
-			storeDeets(p);
-			drawProj(p.currentProjects[i]);
-		};
-
-	}else{
-		p.currentProjects['proj1'] = proj;
-		storeDeets(p);
-		drawProj(proj);
-	}
-
-	
-
-};
-
-function drawProj(proj){
-
-	$('.projects').empty();
-	
-	var draw = 	""+
-				"<div class='project'>"+
-				"<h6>The <strong>" + proj.client + "</strong> Project!</h6>"+
-				"<span><b>Billings: </b>" + proj.billings + "</span><br/>"+
-				"<span><b>Days Left:</b>" + proj.daysLeft + "</span><br/>"+
-				"<span><b>XP:</b>" + proj.xpGiven + "</span>"+
-				"</div>"
-
-	$('.projects').append(draw);
-}
-
+//	Sets up the forget me btn
 function setupForgetMe() {
-
 	$('#forget').on('click',function(){
 		alert('You are forgotten. Refreshing the page');
 		localStorage.clear();
 		location.reload();
 	});
-
 };
 
+//	Sets up the next day btn
 function setupNxtDay(p){
-
 	$('#nxt-day').on('click',function(e){
 		e.stopPropagation();
 		if(!nxtClicked){
@@ -109,11 +63,34 @@ function setupNxtDay(p){
 			nxtClicked = true;
 		}
 	});
+};
 
+//	Important fxn that adds the stringified player pbject to localstorage
+function storeDeets(p){
+	localStorage.setItem('player',JSON.stringify(p));
+};
+
+//Goes in and changes the displayed deets to reflect those passed to it
+function updateDeets(p)	{
+	$('#name').html(p.name);
+	$('#lvl').html(p.lvl);
+	$('#XP').html(p.xp);
+	$('#money').html(p.money).commaAtMeBro().prepend('$');
+	$('#day').html(p.day);
 };
 
 
+/*///////////END SETUP AND STORAGE FUNCTIONS//////////*/
 
+
+
+
+
+
+/*				MOVING THE DAY FORWARD				*/
+
+//	Advance day simply handles taking the btn click from nxtday, updating the fill bar,
+// 	and then calling the endOfDaySummary() which handles all end-day events
 function advanceDay(p){
 
 	var i = 50;
@@ -136,66 +113,145 @@ function advanceDay(p){
 
 };
 
-function randomDayEvents(p){
-
-	//time being
-	// var r = rn(1,3);
-	var r = rn(2,2);
-
-	var c = {};
-	if(r==2){
-		c.proj = new Project(p.lvl);
-		c.con = ""+
-			"<span class='new-proj'>NEW!</span><br/>"+
-			"Looks like you got a new project! Cool!<br/>"+
-			"The company is " + c.proj.client + " and they are offering a handsome " + 
-			c.proj.billings + " for the project which will take " + c.proj.days + "days to complete.<br/>"+
-			"This project awards " + c.proj.xpGiven + "XP upon completion"+
-			"<br/>";
-
-		return c;
-
-	}else{
-		c.con = '';
-		return c;
-	}
-
-};
-
-
+//	Generates the end of the day summary modial
 function endOfDaySummary(p){
 
-	var randomEvent = randomDayEvents(p);
-	showModial(randomEvent.con + 'End of day ' + p.day + '!<br/>');
-	if(randomEvent.proj){
-		updateProjects(p,randomEvent.proj);
+	var isARandomEvent = randomEventGen();
+	var standardMsg = "End of Day" + p.day + "<br/>";
+
+	//	Check to see if we're even supposed to gen a rand evnt
+	if(isARandomEvent) {
+
+		if(Object.keys(p.currentProjects).length < p.devcap){
+			var randomEvent = gimmieARandomEvent(p);
+
+			var newProjName = 'proj' + (Object.keys(p.currentProjects).length + 1);
+
+			p.currentProjects[newProjName] = randomEvent[0];
+
+			var tellModial = randomEvent[1] + standardMsg;
+			showModial(tellModial,p);
+
+		}else{
+			standardMsg += "What a great day to be developing!<br/>";
+			showModial(standardMsg,p);
+		}	
+
 	}else{
-		updateProjects(p);
+		showModial(standardMsg,p);
 	}
-	p.day++
-	updateDeets(p);
-	$("#nxt-fill").css('width','100%');
-	storeDeets(p);
-	nxtClicked = false;
 
 };
 
+//	Both generates a new project (doesnt handle adding to currentProjects) and the text
+//	for the modial that goes along with it
+function gimmieARandomEvent(p)	{
 
-function btnSetup(p) {
-	setupForgetMe();
-	setupNxtDay(p);
-};	
+	var evnt = [];
+	evnt[0] = new Project(p.lvl);
+	evnt[1] = 	""+
+				"<span class='new-proj'>NEW!</span><br/>"+
+				"Looks like you got a new project! Cool!<br/>"+
+				"The company is " + evnt[0].client + " and they are offering a handsome " + 
+				evnt[0].billings + " for the project which will take " + evnt[0].days + "days to complete.<br/>"+
+				"This project awards " + evnt[0].xpGiven + "XP upon completion"+
+				"<br/>";
+	return evnt
 
-function showModial(msg){
+};
+
+//	Determines if a random event should be generated or not
+function randomEventGen()	{
+	var r = rn(1,3);	
+	if(r==2){
+		return true
+	}else{
+		return false
+	}
+};
+
+/*				SHOWING AND HIDING THE MODIAL						*/
+
+//	Takes a message (html welcome) and adds it to the modial which is added to the page
+function showModial(msg,p){
 	var a = "<div class='modial'><div class='inner-modial'>";
 	var b = "<div id='close-modial' class='btn'>Close</div></div></div>";
 	var mod = a+msg+b;
 	$('body').append(mod);
 	$('#close-modial').on('click',function(e){
 		e.stopPropagation();
-		$('.modial').remove();
+		closeModial(p);
 	});
 };
+
+//	Closes the modial, advances the day forward, and gets shit ready for the next day
+function closeModial(p) {
+
+	$('.modial').remove();
+	$("#nxt-fill").css('width','100%');
+	p.day++
+
+	if(!$.isEmptyObject(p.currentProjects)){
+		decrementProjectDaysLeft(p);
+	}
+	updateProjects(p);
+	updateDeets(p);
+	storeDeets(p);
+	nxtClicked = false;
+};
+
+/*				SHOWING AND HIDING THE MODIAL						*/
+
+
+/*				END MOVING THE DAY FORWARD				*/
+
+
+/*				UPDATING PROJECTS						*/
+
+//	Loops through all projects (if there are some to loop through) and draws that project, 
+function updateProjects(p){
+	
+	//check if user has any current projects goin on
+	if(!$.isEmptyObject(p.currentProjects)){
+
+		var len = Object.keys(p.currentProjects).length;
+		$('.projects').empty();
+		for (k = 1; k <= len; k++){
+			var i = 'proj' + k;	
+			drawProj(p.currentProjects[i]);
+		};
+
+	}
+
+};
+
+
+//	Simply intended to draw a single project (not a lot of logic)
+function drawProj(proj){
+	var draw = 	""+
+				"<div class='project'>"+
+				"<h6>The <strong>" + proj.client + "</strong> Project!</h6>"+
+				"<span><b>Billings: </b>" + proj.billings + "</span><br/>"+
+				"<span><b>Days Left:</b>" + proj.daysLeft + "</span><br/>"+
+				"<span><b>XP:</b>" + proj.xpGiven + "</span>"+
+				"</div>"
+
+	$('.projects').append(draw);
+};
+
+
+function decrementProjectDaysLeft(p){
+
+	var len = Object.keys(p.currentProjects).length;
+	for (k = 1; k <= len; k++){
+		var i = 'proj' + k;	
+		p.currentProjects[i].daysLeft--
+	}
+
+};
+
+/*				END UPDATING PROJECTS					*/
+
 
 
 
